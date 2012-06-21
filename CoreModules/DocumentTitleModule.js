abc96a84d8f925a42a404792f80e7df013ee7401
Aran Mulholland <aranmulholland@gmail.com>
2012-06-21 12:15:13 +1000

    Added a basic document title module that listens for notifications and updates the document title

---
+++ CoreModules/DocumentTitleModule.js
@@ -0,0 +1,15 @@
+﻿Core.register("DocumentTitleModule", function (sandbox) {
+
+   var changeTitle = function (newTitle) {
+      sandbox.setDocumentTitle(newTitle);
+   };
+
+   return {
+      activate: function () {
+         sandbox.addListener("DocumentTitle", changeTitle);
+      },
+      destroy: function () {
+         sandbox.removeListener("DocumentTitle", changeTitle);
+      }
+   };
+});
