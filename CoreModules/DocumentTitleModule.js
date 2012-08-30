75160345e7f74ac04d1ac32d902fbe092eacbc3a
Aran Mulholland <aranmulholland@gmail.com>
2012-08-30 11:16:07 +1000

    Added a document title module for setting the browser title for a page

--- CoreModules/DocumentTitleModule.js
+++ CoreModules/DocumentTitleModule.js
@@ -1,15 +1,27 @@
 ﻿Core.register("DocumentTitleModule", function (sandbox) {
 
-   var changeTitle = function (newTitle) {
-      sandbox.setDocumentTitle(newTitle);
-   };
+   var title = "",
+       suffix = "",
+       changeTitle = function () {
+          sandbox.setDocumentTitle(title + " " + suffix);
+       },
+       setTitle = function (newTitle) {
+          title = newTitle;
+          changeTitle();
+       },
+       setSuffix = function (additionalSuffix) {
+          suffix = additionalSuffix;
+          changeTitle();
+       };
 
    return {
       activate: function () {
-         sandbox.addListener("DocumentTitle", changeTitle);
+         sandbox.addListener("DocumentTitle", setTitle);
+         sandbox.addListener("DocumentTitleSuffix", setSuffix);
       },
       destroy: function () {
          sandbox.removeListener("DocumentTitle", changeTitle);
+         sandbox.removeListener("DocumentTitleSuffix", setSuffix);
       }
    };
 });
